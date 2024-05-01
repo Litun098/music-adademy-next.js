695ef189c5d7888cd1068b0f8f13aae847fea929
litun098 <nayaklitun9@gmail.com>
2024-05-01 23:21:52 +0530

    added whyChooseUs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <HeroSection/>
-      <FeaturedCourses/>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] overflow-auto">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
       <Footer />
     </main>
   );
 }
-
-// Solving Hero Section
